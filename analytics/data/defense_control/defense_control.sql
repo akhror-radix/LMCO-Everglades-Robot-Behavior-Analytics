c2f1a530935edb25106f4f2bf38cdfede37202a0
cfepes <chandlerepes@gmail.com>
2020-03-24 19:03:14 -0400

    Restructured datasets folder, units_remaining dataset created

---
+++ analytics/data/defense_control/defense_control.sql
@@ -0,0 +1,19 @@
+SELECT *, IF(S.p0_score > S.p1_score, 0, 1)
+FROM Games
+INNER JOIN Scores
+ON Games.game_id    = Scores.game_id AND
+   Games.turn_count = Scores.turn_num 
+WHERE Games.win_state = 1
+UNION
+SELECT *, IF(N.control_percent = -100, 1, 0)
+FROM Games
+INNER JOIN Node_Knowledge
+ON Games.turn_count = Node_Knowledge.turn_num AND
+   Games.game_id    = Node_Knowledge.game_id
+WHERE Games.win_state = 2 AND
+      Node_Knowledge.node_id = 1 AND
+      Node_Knowledge.player = 1
+UNION
+SELECT *, -1
+FROM Games
+WHERE Games.win_state = 3
---
+++ analytics/data/defense_control/specifications.md
@@ -0,0 +1,7 @@
+# defense_control
+defense_control is a dataset that describes the average percentage control each player had over the fortress node on the map.
+
+## Form
+| p0_avg_control | p1_avg_control | turn_count | winstate | winner |
+| -------------- | -------------- | ---------- | -------- | ------ |
+| .65 | .25 | 150 | 1 | 1 |
